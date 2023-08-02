e2a85e5d218b8a083e57b69209690e83202be517
VKMorgot <solerosa@mail.ru>
2023-08-03 00:44:15 +0300

    Обновление GetProductsTest
    - тест обновлен (2/6)

--- src/test/java/lesson6/GetProductTest.java
+++ src/test/java/lesson6/GetProductTest.java
@@ -1,13 +1,16 @@
 package lesson6;
 
+import lesson6.db.dao.ProductsMapper;
+import lesson6.db.model.ProductsExample;
 import lesson6.dto.Product;
 import lombok.SneakyThrows;
+import org.apache.ibatis.session.SqlSession;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetProductTest extends CommonProductTest {
 
@@ -25,6 +28,21 @@ public class GetProductTest extends CommonProductTest {
         assertThat(response.body().getId(), CoreMatchers.is(id));
         assertThat(response.body().getCategoryTitle(), CoreMatchers.equalTo("Food"));
         assertThat(response.body().getTitle(), CoreMatchers.equalTo("Milk"));
+
+        //проверяем, что в БД с таким же id находится этот же продукт с такими же параметрами
+        try (SqlSession session = getSqlSessionFactory().openSession()) {
+
+            ProductsMapper productsMapper = session.getMapper(ProductsMapper.class);
+            ProductsExample example = new ProductsExample();
+
+            example.createCriteria()
+                    .andCategory_idEqualTo((long) 1)       // id=1 категории = Food
+                    .andTitleEqualTo("Milk");
+
+            assertThat(productsMapper.countByExample(example), equalTo(1L)); // по id найден один продукт
+            assertThat(productsMapper.selectByExample(example).get(0).getTitle(), equalTo("Milk"));  // имя продукта
+            assertThat(productsMapper.selectByExample(example).get(0).getCategory_id(), equalTo(1L));  // принадлежность к категории
+        }
     }
 
     /**
